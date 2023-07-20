b063db4be6f9d1289f291f126b732523b6cf4730
JuanTosso <juantosso89@gmail.com>
2023-07-20 19:57:23 -0300

    editar perfil y ver perfil de terceros

--- client/src/components/views/Profile.jsx
+++ client/src/components/views/Profile.jsx
@@ -1,12 +1,12 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import NavBar from "./NavBar.jsx";
 import { getUserById } from "../../Redux trad/actions.js";
-import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 const Profile = () => {
   const user = useSelector((state) => state.user);
-  const userImg = user.image;
+  const { userName, image, bio } = user;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,21 +19,16 @@ const Profile = () => {
         <div className="max-w-lg mx-auto py-5 bg-grey rounded-lg md:shadow-md p-5 font-quick">
           <img
             className="w-32 h-32 rounded-full mx-auto"
-            src={userImg}
+            src={image}
             alt="Profile picture"
           />
           <h2
             className="text-center text-2xl font-semibold mt-3"
             style={{ color: "#000000" }}
           >
-            {user.userName}
+            {userName}
           </h2>
-          <p
-            className="text-center text-gray-600 mt-1"
-            style={{ color: "#000000" }}
-          >
-            Football player (The best)
-          </p>
+
           <div className="flex justify-center mt-5">
             <a
               href="https://www.instagram.com"
@@ -71,7 +66,7 @@ const Profile = () => {
               Bio:
             </h3>
             <p className="text-gray-600 mt-2" style={{ color: "#000000" }}>
-              CR7 the best player in the world.
+              {bio}
             </p>
           </div>
           <div className="mt-5 text-center">
