e701fd6c18e0f71f20ec74c4b996dda8008a7e21
David Paimon <davidfelipe.fj@gmail.com>
2023-07-21 10:37:31 -0500

    Footer más pequeño

--- client/src/components/landing/Recomend.jsx
+++ client/src/components/landing/Recomend.jsx
@@ -22,19 +22,19 @@ const Recomend = () => {
   centerSlidePercentage={25}
   showIndicators={false}
 >
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/cine_mhcjjd.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/baloncesto_yivlo2.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/Estudio_tqil81.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/paracaidismo_liey90.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951926/Nomadlocals/Landing/restaurante_dhls56.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/museos_g1qtaj.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/fogata_vcdm7u.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951924/Nomadlocals/Landing/futbol11_faabet.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/picnic_fg5dk8.webp" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/paintBall_qoguuy.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/piscina_asphew.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951926/Nomadlocals/Landing/videojuegos_mc6nax.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
-  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951926/Nomadlocals/Landing/playa_aa1hf8.png" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/cine_mhcjjd.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="cine" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/baloncesto_yivlo2.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="baloncesto" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/Estudio_tqil81.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="estudio" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/paracaidismo_liey90.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="paracaidismo" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951926/Nomadlocals/Landing/restaurante_dhls56.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="restaurante" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/museos_g1qtaj.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="museos" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951923/Nomadlocals/Landing/fogata_vcdm7u.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="fogata" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951924/Nomadlocals/Landing/futbol11_faabet.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="futbol11" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/picnic_fg5dk8.webp" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="picnic" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/paintBall_qoguuy.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="paintball" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951925/Nomadlocals/Landing/piscina_asphew.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="piscina" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951926/Nomadlocals/Landing/videojuegos_mc6nax.jpg" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="videojuegos" />
+  <img src="https://res.cloudinary.com/dwit2djhy/image/upload/v1689951926/Nomadlocals/Landing/playa_aa1hf8.png" className="w-full h-auto rounded-lg shadow-lg p-3 min-h-40 sm:w-40" alt="playa" />
 </Carousel>
 
     </div>
