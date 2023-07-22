d4314d7f5cbc9076cf943759b67960d361beca7c
JuanTosso <juantosso89@gmail.com>
2023-07-22 14:49:43 -0300

    dashboard

--- client/src/components/AdminComponents/AllUsers/AllUsers.jsx
+++ client/src/components/AdminComponents/AllUsers/AllUsers.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { getAllUsers } from "../../../Redux trad/actions.js";
 import { useDispatch, useSelector } from "react-redux";
-// import { cleanData} from "../../../redux/actions/actionBooks";
 import { Link } from "react-router-dom";
 import NavBar from "../../views/NavBar.jsx";
+import s from "./AllUsers.module.css";
+
+// import { cleanData} from "../../../redux/actions/actionBooks";
 // import profile from "../../../assets/images/avatar2.png";
 // import Pagination from "../../CommonComponents/Pagination/Pagination";
-import s from "./AllUsers.module.css";
-// import Remove from "../../../Iconos/Remove.jsx";
-// import Edit from "../../../Iconos/Edit.jsx";
+import Remove from "../../../iconos/Remove.jsx";
+import Edit from "../../../iconos/Edit.jsx";
 
 // import swal from "sweetalert";
 
@@ -16,8 +17,8 @@ function AllUsers() {
   const dispatch = useDispatch();
   const allUsers = useSelector((state) => state.allUsers);
   const userActu = useSelector((state) => state.user);
-  const adminState = userActu.admin;
-
+  const adminState = !userActu.admin;
+  console.log(allUsers);
   useEffect(() => {
     dispatch(getAllUsers());
 
@@ -26,38 +27,38 @@ function AllUsers() {
     // };
   }, []);
 
-  const handleEdit = (e, id, moderador) => {
+  const handleEdit = (e, id, administrador) => {
     e.preventDefault();
-    console.log("id es ", id, moderador);
+    // console.log("id es ", id, administrador);
 
-    if (userActu._id === id) {
-      swal("No puedes editarte a ti mismo!");
-    } else {
-      if (moderador !== false) {
-        swal("No podes quitarle el permiso de administrador a este usuario");
-      } else {
-        dispatch(adminAnUser({ id: id, moderador: true }));
-        swal("Ahora el usuario tiene permisos de administrador!");
-      }
-    }
+    // if (userActu._id === id) {
+    //   swal("No puedes editarte a ti mismo!");
+    // } else {
+    //   if (administrador !== false) {
+    //     swal("No podes quitarle el permiso de administrador a este usuario");
+    //   } else {
+    //     dispatch(adminAnUser({ id: id, administrador: true }));
+    //     swal("Ahora el usuario tiene permisos de administrador!");
+    //   }
+    // }
   };
 
   const handleDelete = async (e, id) => {
     e.preventDefault();
-    if (userActu._id === id) {
-      swal("No puedes eliminarte a ti mismo");
-    } else {
-      if (
-        window.confirm(
-          "¿Estás seguro que quieres eliminar este usuario? Si lo eliminas, no podrás deshacer esta acción."
-        ) === true
-      ) {
-        dispatch(deleteUser(id));
-        swal("Usuario eliminado correctamente.");
+    // if (userActu._id === id) {
+    //   swal("No puedes eliminarte a ti mismo");
+    // } else {
+    //   if (
+    //     window.confirm(
+    //       "¿Estás seguro que quieres eliminar este usuario? Si lo eliminas, no podrás deshacer esta acción."
+    //     ) === true
+    //   ) {
+    //     dispatch(deleteUser(id));
+    //     swal("Usuario eliminado correctamente.");
 
-        window.location.reload();
-      }
-    }
+    //     window.location.reload();
+    //   }
+    // }
   };
   return (
     <div>
@@ -65,53 +66,51 @@ function AllUsers() {
 
       {adminState ? (
         <div>
-          <Link to="/Admin">
+          <Link to="/admin">
             <button className="btnAtras">Atrás</button>
           </Link>
           <div className={s.container}>
-            <table class="listado">
+            <table className="listado">
               <thead className="tituloTabla">
                 <tr>
                   <th>NOMBRE</th>
                   <th>IMAGEN</th>
-                  <th>VERIFICADO</th>
+                  <th>EMAIL</th>
                   <th>BLOQUEADO</th>
-                  <th>MODERADOR</th>
-                  <th colspan="2">OPCIONES</th>
+                  <th>ADMINISTRADOR</th>
+                  <th colSpan="2">OPCIONES</th>
                 </tr>
               </thead>
               <tbody>
-                {allUsers?.map((u, i) => {
-                  console.log("a ver qué trae", u);
+                {allUsers?.map((u) => {
                   return (
-                    <tr key={i} className={s.containerInfo}>
-                      <td className={s.id}>{u.id}</td>
-                      <td className={s.name}>{u.userName}</td>
+                    <tr key={u.id}>
+                      <td>{u.userName}</td>
                       <td>
                         <img
                           className="image-e"
-                          src={u.image.url || profile}
+                          src={u.image}
                           alt="No disponible"
                         />
                       </td>
-                      <td className={s.verified}>{u.verified}</td>
-                      <td className={s.moderator}>
-                        {u.moderador ? "SÍ" : "NO"}
-                      </td>
-                      <td className={s.blocked}>{u.blocked}</td>
-                      <td className={s.actions}>
+                      <td>{u.email}</td>
+                      {/* falta hacer el blocked en el modelo */}
+                      <td>{u.blocked}</td>
+                      <td>{u.admin ? "SÍ" : "NO"}</td>
+
+                      <td>
                         <button
-                          onClick={(e) => handleEdit(e, u.id, u.moderador)}
+                          onClick={(e) => handleEdit(e, u.id, u.admin)}
                           className="btn-edita"
                         >
-                          <Edit />
+                          {/* <Edit /> */}
                         </button>
 
                         <button
                           onClick={(e) => handleDelete(e, u.id)}
                           // className={s.deleteBtn}
                         >
-                          <Remove />
+                          {/* <Remove /> */}
                         </button>
                       </td>
                     </tr>
@@ -120,7 +119,7 @@ function AllUsers() {
               </tbody>
             </table>
           </div>
-          <Pagination />
+          {/* <Pagination /> */}
         </div>
       ) : null}
     </div>
