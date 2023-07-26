d087a7b0755f4b92a57d1cca415ddbaff2e4647a
JuanTosso <juantosso89@gmail.com>
2023-07-26 14:43:35 -0300

    boton reset filters

--- client/src/components/views/Home.jsx
+++ client/src/components/views/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Footer from "./Footer.jsx";
 import SuggestionCarousel from "./SuggestionCarousel.jsx";
 import { useSelector, useDispatch } from "react-redux";
-import { getUserActivities, getUserById } from "../../Redux trad/actions.js";
+import { getUserActivities, resetFilters } from "../../Redux trad/actions.js";
 import { useEffect, useState } from "react";
 
 const Home = () => {
@@ -14,6 +14,7 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(getUserActivities(user.id));
+    dispatch(resetFilters());
   }, []);
 
   //Evitar ingreso de usuarios banneados:
@@ -23,6 +24,7 @@ const Home = () => {
     const delay = 3000;
     const timerId = setTimeout(() => {
       // Verificar si el usuario está suspendido después del retraso
+      console.log(user);
       if (!(user && "deletedAt" in user)) {
         setIsUserSuspended(true);
       }
@@ -69,9 +71,9 @@ const Home = () => {
       <div className="flex justify-end pr-2 md:pr-5 xl:pr-10 mt-4">
         {isAdmin ? (
           <Link to="/admin">
-          <button className="text-white font-quick p-2 rounded-lg bg-blue shadow-lg ring-1 ring-black ring-opacity-5 max-w-md hover:scale-110 ease-out duration-300">
-            Admin Panel
-          </button>
+            <button className="text-white font-quick p-2 rounded-lg bg-blue shadow-lg ring-1 ring-black ring-opacity-5 max-w-md hover:scale-110 ease-out duration-300">
+              Admin Panel
+            </button>
           </Link>
         ) : (
           ""
@@ -82,10 +84,10 @@ const Home = () => {
         Tus Actividades:
       </h1>
       <div className="flex flex-col items-center text-white content-around py-5 px-2 md:px-5 xl:px-10 xl:pt-10 bg-grey font-spartan text-lg md:flex-row md:justify-around">
-      <Link to="/activity-form">
-        <button className="p-2 rounded-lg bg-blue shadow-lg ring-1 ring-black ring-opacity-5 md:w-56 lg:w-80 lg:h-20 lg:text-2xl hover:scale-110 ease-out duration-300">
-          Crea tu actividad
-        </button>
+        <Link to="/activity-form">
+          <button className="p-2 rounded-lg bg-blue shadow-lg ring-1 ring-black ring-opacity-5 md:w-56 lg:w-80 lg:h-20 lg:text-2xl hover:scale-110 ease-out duration-300">
+            Crea tu actividad
+          </button>
         </Link>
       </div>
       <section className="pt-5 px-4 lg:pt-[80px] pb-10 lg:pb-20 bg-grey">
@@ -97,9 +99,9 @@ const Home = () => {
         </h1>
         <SuggestionCarousel />
         <Link to="/activities">
-        <button className="p-2 rounded-lg bg-blue text-white my-4 shadow-lg ring-1 ring-black ring-opacity-5 font-spartan lg:w-80 lg:h-20 lg:text-2xl hover:scale-110 ease-out duration-300">
-          Encuentra una actividad
-        </button>
+          <button className="p-2 rounded-lg bg-blue text-white my-4 shadow-lg ring-1 ring-black ring-opacity-5 font-spartan lg:w-80 lg:h-20 lg:text-2xl hover:scale-110 ease-out duration-300">
+            Encuentra una actividad
+          </button>
         </Link>
       </section>
       <Footer />
